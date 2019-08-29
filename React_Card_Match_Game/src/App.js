13177db99abe34537c4a53125068cc768bb30921
JacoMig <jacomig@gmail.com>
2019-08-29 18:57:02 +0200

    Add GameOver, Countdown, clean Code

--- React_Card_Match_Game/src/App.js
+++ React_Card_Match_Game/src/App.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react'
 let checkers = [] 
 let completed = []
 let isMatch = false
+let isMounted = false
+let click = 0;
 const images = {
     angular: "img/angular.png",
     css: "img/css.png",
     go: "img/go.png",
-    html: "img/html.png",
+   /*  html: "img/html.png",
     rail: "img/rail.png",
     react: "img/react.png",
     scala: "img/scala.png",
-    vue: "img/vue.png"
+    vue: "img/vue.png" */
 }
 
 const buildCards = () => {
     const Cards = [];
     let id = 0;
 
-    const duplicateCards = (images) => {
+    const createCards = (images) => {
         for(var name in images){
             const imageurl = images[name]
             id++
@@ -33,53 +35,105 @@ const buildCards = () => {
             )
         }
     }
-    duplicateCards(images)
-    duplicateCards(images)
+    createCards(images)
+    createCards(images)
    
     Cards.sort(() =>  Math.random() - 0.5 )
 
     return Cards;
 }
 
+
+
 const RenderCards = (props) => {
     const { front_img, back_img, flipped, id, type } = props.card
     const img_src = flipped ? front_img : back_img;
-     
     return <div className="Card" data-flipped={flipped} data-type={type} id={id} key={id} onClick={props.clickHandler} >
                 <img src={img_src} />
             </div>
 }
 
 class App extends Component{
-    constructor(){
-        super()
+    
+    constructor(props){
+        super(props)
         this.state = ({
-            Cards: buildCards()
+            Cards: buildCards(),
+            timer: { min: 2, sec: 60 }
         })
     }
     
-    
+    componentDidMount(){
+        isMounted = true
+        completed = []
+        isMatch = false
+
+        var countDown = setInterval( () => {
+            if(isMounted){
+                if( this.state.timer.min > 0 && this.state.timer.sec === 1)
+                    this.setState({timer : { min: this.state.timer.min-=1 , sec: 60 } })
+                else    
+                    this.setState({timer : { min: this.state.timer.min,  sec: this.state.timer.sec-=1 } })
+                if( (this.state.timer.min === 0 && this.state.timer.sec === 0) ){
+                    this.props.ReStart()
+                    isMounted = false
+                }
+            }else{
+                clearInterval(countDown)
+            }
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        isMounted = false
+    }
+
     clickHandler(e){
         e.preventDefault()
-        const Cards = this.state.Cards;
+        // const Cards = this.state.Cards;
         const Id = e.target.parentNode.id;
-        let newCards = Cards;
+        let newCards = this.state.Cards;
         const currentCard = newCards.find( (el) => el.id == Id );
         
-        if(checkersFull(checkers) || cardAlreadyMatched(currentCard, completed) ) return
+        if(checkersFull(checkers) || cardAlreadyMatched(currentCard, completed) || click >= 2) return
         
-        console.log("init click handler")
-       
+        console.log("INIT CLICK HANDLER")
+        
+        click++
         currentCard.flipped = true
         checkers.push(currentCard);
         this.setState({ Cards: newCards }) 
         
+        // VALIDATE CHOOSEN CARDS and SET A MATCH IF NEEDED
         if(validateCheckers(checkers)){
             isMatch = true
             if(!cardAlreadyMatched(currentCard, completed))
                 completed.push(checkers[0].type)
-        }else{
+         }else{
             isMatch = false
+        }
+
+        if(checkers.length >= 2){
+            // CHECK IF IS A MATCH
+            setTimeout( () => {
+                if(!isMatch && isMounted){
+                    this.state.Cards.map(card => { 
+                        if(!completed.includes(card.type))
+                            card.flipped = false
+                    })
+                    this.setState({ Cards: newCards });
+                }
+                click = 0
+            }, 1000 )   
+            console.log("Is Match: "+isMatch)
+            checkers = []
+        }
+        
+        // WIN 
+        if(completed.length === Object.entries(images).length ){
+            this.props.GameWin() 
+            isMounted = false
+            console.log('game won')
         }
 
         function checkersFull(checkers){
@@ -92,32 +146,20 @@ class App extends Component{
 
         function cardAlreadyMatched(currentCard, completed){
             return completed.includes(currentCard.type)
-        }  
-
-       
-        if(checkers.length >= 2){
-            checkers = []
-        }
-        
-        setTimeout( () => {
-            if(!isMatch){
-                currentCard.flipped = false;
-                this.setState({ Cards: newCards });
-                checkers = []
-            }
-        }, 1000 )   
-        
-
-        console.log("Is Match: "+isMatch)
+        } 
 
     }
 
     render(){
         return (
             <div>
+                <div className="timer">{this.state.timer.min}:{this.state.timer.sec}</div>
+                <div className="gridCards">
                 { this.state.Cards.map((card) => <RenderCards key={card.id}  card={card} clickHandler={this.clickHandler.bind(this)} />) }    
+                </div>
             </div>
         )
+       
     }
 }
 
--- React_Card_Match_Game/src/index.js
+++ React_Card_Match_Game/src/index.js
@@ -2,5 +2,24 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+class CardGame extends Component{
+    constructor(){
+        super();
+        this.state = { start : false, gameWin: false }
+    }
+    render(){
+        if( this.state.start && !this.state.gameWin ){
+            return  <App ReStart={() => this.setState({start:false})} GameWin={ () => this.setState({gameWin:true}) } />
+        }
+        if( !this.state.start && !this.state.gameWin ){
+            return <button onClick={() => this.setState({start:true, gameWin: false})}>Start Game</button>
+        }
+        if( this.state.gameWin ){
+            return <div>
+                        <h2>You won!</h2><button onClick={() => this.setState({start:true, gameWin: false})}>Start Game</button>
+                    </div>
+        }
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<CardGame />, document.getElementById('app'))
