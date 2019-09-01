67e0f6bcd190fd773ea99013e20e53866df23aa6
JacoMig <jacomig@gmail.com>
2019-09-01 17:31:52 +0200

    Add Sequencer, switch between sounds

--- Tonejs/scripts.js
+++ Tonejs/scripts.js
@@ -9,22 +9,66 @@ document.documentElement.addEventListener('mousedown', () => {
 const play = document.getElementById('play')
 const stop = document.getElementById('stop')
 const slider = document.getElementById('Slider')
+const lights = document.querySelector('.lights')
 
-Tone.Transport.bpm.value = 80;
+const kickBtn = document.querySelector('[data-type="kick"]')
+const hihatBtn = document.querySelector('[data-type="hihat"]')
 
-var synth = new Tone.Synth().synth.toMaster();
+let bpm = 120
+Tone.Transport.bpm.value = bpm;
 
-var loop = new Tone.Loop(function(time){
+const synth = new Tone.Synth();
+synth.oscillator.type = 'triangle';
+
+const gain = new Tone.Gain(0.6);
+gain.toMaster();
+synth.connect(gain)
+
+const kickSound = new Tone.Player("./samples/Kick909.wav").connect(gain)
+const hihatSound = new Tone.Player("./samples/closedhh909.wav").connect(gain)
+
+let Sound = kickSound;
+
+
+let index = 0
+const row = document.getElementById('Sequencer')
+Tone.Transport.scheduleRepeat(repeat, '16n');
+
+function repeat(time){
+    let step = index % 16;
+    
+    const $input = row.querySelector(`input:nth-child(${step + 1})`);
+    lights.querySelector(`div:nth-child(${step + 1})`).classList.add('on')
+     if(step === 0)
+      lights.querySelector(`div:nth-child(16)`).classList.remove('on')
+    else
+      lights.querySelector(`div:nth-child(${step})`).classList.remove('on')
+    console.log(step)
+    console.log(lights.querySelector(`div:nth-child(${step + 1})`)) 
+    
+    if($input.checked) {
+      console.log($input);
+      //synth.triggerAttackRelease('C4', 0.2)
+      Sound.start()
+    }
+    index++
+}
+
+/* var loop = new Tone.Loop(function(time){
 	synth.triggerAttackRelease('C4', 0.2)
   
   
-}, "2n").start(0);
+}, "16n").start(0); */
 
 //Tone.Transport.start();
 
 play.addEventListener("click", () => { Tone.Transport.start(); })
 stop.addEventListener("click", () => { Tone.Transport.stop(); })
 slider.addEventListener("input", (e) => { 
-  synth.envelope.release = e.target.value * 0.1
+  // synth.envelope.release = e.target.value * 0.1
+  bpm = e.target.value
+  Tone.Transport.bpm.value = bpm;
 }) 
+kickBtn.addEventListener("click", (e) => { Sound = kickSound; })
+hihatBtn.addEventListener("click", (e) => { Sound = hihatSound; })
 
